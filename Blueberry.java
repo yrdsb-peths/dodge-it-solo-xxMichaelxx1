a3c3aa2cd3e4022d2f6280ab0f016c7c9921938a
xxMichaelxx1 <woushinidie@gmail.com>
2025-04-16 09:28:41 -0400

    Reset Blueberry

--- Blueberry.java
+++ Blueberry.java
@@ -6,5 +6,18 @@ public class Blueberry extends Actor
     public void act()
     {
         move(-3);
+        
+        if(getX() <= 0) {
+            resetBlueberry();
+        }
+    }
+    
+    public void resetBlueberry(){
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0) {
+            setLocation(600, 100);
+        } else {
+            setLocation(600, 300);
+        }
     }
 }
